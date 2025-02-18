e993bf7c109fd2d0e1a5c7599dc9e68907316985
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2025-02-18 09:24:46 +0100

    using recat template for pricing

--- my-project/src/pages/Pricing/PricingPage.tsx
+++ my-project/src/pages/Pricing/PricingPage.tsx
@@ -1,8 +1,40 @@
+import {Check} from '@mui/icons-material';
+
+const tiers = [
+	{
+		name: 'Hobby',
+		id: 'tier-hobby',
+		href: '#',
+		priceMonthly: '$29',
+		description: "The perfect plan if you're just getting started with our product.",
+		features: ['25 products', 'Up to 10,000 subscribers', 'Advanced analytics', '24-hour support response time'],
+		featured: false,
+	},
+	{
+		name: 'Enterprise',
+		id: 'tier-enterprise',
+		href: '#',
+		priceMonthly: '$99',
+		description: 'Dedicated support and infrastructure for your company.',
+		features: ['Unlimited products', 'Unlimited subscribers', 'Advanced analytics', 'Dedicated support representative', 'Marketing automations', 'Custom integrations'],
+		featured: true,
+	},
+];
+
+function classNames(...classes: string[]) {
+	return classes.filter(Boolean).join(' ');
+}
 const PricingPage = () => {
 	return (
 		<div>
-			<div className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
-				<div className="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"></div>
+			<div aria-hidden="true" className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl">
+				<div
+					style={{
+						clipPath:
+							'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+					}}
+					className="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+				/>
 			</div>
 			<div className="mx-auto max-w-4xl text-center">
 				<h2 className="text-base/7 font-semibold text-indigo-600">Pricing</h2>
@@ -12,142 +44,43 @@ const PricingPage = () => {
 				Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer loyalty, and driving sales.
 			</p>
 			<div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-				<div className="rounded-3xl rounded-t-3xl bg-white/60 p-8 ring-1 ring-gray-900/10 sm:mx-8 sm:rounded-b-none sm:p-10 lg:mx-0 lg:rounded-tr-none lg:rounded-bl-3xl">
-					<h3 id="tier-hobby" className="text-base/7 font-semibold text-indigo-600">
-						Hobby
-					</h3>
-					<p className="mt-4 flex items-baseline gap-x-2">
-						<span className="text-5xl font-semibold tracking-tight text-gray-900">$29</span>
-						<span className="text-base text-gray-500">/month</span>
-					</p>
-					<p className="mt-6 text-base/7 text-gray-600">The perfect plan if you&#039;re just getting started with our product.</p>
-					<ul role="list" className="mt-8 space-y-3 text-sm/6 text-gray-600 sm:mt-10">
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							25 products
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Up to 10,000 subscribers
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Advanced analytics
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							24-hour support response time
-						</li>
-					</ul>
-					<a
-						href="#"
-						aria-describedby="tier-hobby"
-						className="mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:mt-10">
-						Get started today
-					</a>
-				</div>
-				<div className="relative rounded-3xl bg-gray-900 p-8 ring-1 shadow-2xl ring-gray-900/10 sm:p-10">
-					<h3 id="tier-enterprise" className="text-base/7 font-semibold text-indigo-400">
-						Enterprise
-					</h3>
-					<p className="mt-4 flex items-baseline gap-x-2">
-						<span className="text-5xl font-semibold tracking-tight text-white">$99</span>
-						<span className="text-base text-gray-400">/month</span>
-					</p>
-					<p className="mt-6 text-base/7 text-gray-300">Dedicated support and infrastructure for your company.</p>
-					<ul role="list" className="mt-8 space-y-3 text-sm/6 text-gray-300 sm:mt-10">
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Unlimited products
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Unlimited subscribers
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Advanced analytics
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Dedicated support representative
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Marketing automations
-						</li>
-						<li className="flex gap-x-3">
-							<svg className="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-								<path
-									fill-rule="evenodd"
-									d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							Custom integrations
-						</li>
-					</ul>
-					<a
-						href="#"
-						aria-describedby="tier-enterprise"
-						className="mt-8 block rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 sm:mt-10">
-						Get started today
-					</a>
-				</div>
+				{tiers.map((tier, tierIdx) => (
+					<div
+						key={tier.id}
+						className={classNames(
+							tier.featured ? 'relative bg-gray-900 shadow-2xl' : 'bg-white/60 sm:mx-8 lg:mx-0',
+							tier.featured ? '' : tierIdx === 0 ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl' : 'sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none',
+							'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10'
+						)}>
+						<h3 id={tier.id} className={classNames(tier.featured ? 'text-indigo-400' : 'text-indigo-600', 'text-base/7 font-semibold')}>
+							{tier.name}
+						</h3>
+						<p className="mt-4 flex items-baseline gap-x-2">
+							<span className={classNames(tier.featured ? 'text-white' : 'text-gray-900', 'text-5xl font-semibold tracking-tight')}>{tier.priceMonthly}</span>
+							<span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-500', 'text-base')}>/month</span>
+						</p>
+						<p className={classNames(tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-6 text-base/7')}>{tier.description}</p>
+						<ul role="list" className={classNames(tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-8 space-y-3 text-sm/6 sm:mt-10')}>
+							{tier.features.map((feature) => (
+								<li key={feature} className="flex gap-x-3">
+									<Check aria-hidden="true" className={classNames(tier.featured ? 'text-indigo-400' : 'text-indigo-600', 'h-6 w-5 flex-none')} />
+									{feature}
+								</li>
+							))}
+						</ul>
+						<a
+							href={tier.href}
+							aria-describedby={tier.id}
+							className={classNames(
+								tier.featured
+									? 'bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500'
+									: 'text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-indigo-600',
+								'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10'
+							)}>
+							Get started today
+						</a>
+					</div>
+				))}
 			</div>
 		</div>
 	);
